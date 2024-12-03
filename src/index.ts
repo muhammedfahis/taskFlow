d840225df3a91f63a2171940ae22a0d3b5dac87c
muhammedfayiscc <mohammed.fayis@pegasusone.com>
2024-12-03 23:40:19 +0530

    final commit

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 (async () => {    
-    if(!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
-    }
     if(!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
